1c58c00a00f1134489c7d290f9cc794177116823
Nicholas Galante <ngalant2@binghamton.edu>
2020-05-05 11:00:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find(ifnone = nil){ |string| string == valid_calls }
+  planeteer_calls.find(ifnone = nil){ |string| string == valid_calls[0] || string == valid_calls[1] || string == valid_calls[2] || string == valid_calls[3] || string == valid_calls[4] }
 end
